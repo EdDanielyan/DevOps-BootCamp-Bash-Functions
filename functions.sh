faf931b447d411e691e232354ceb093fc1445cfe
EdDanielyan <102631130+EdDanielyan@users.noreply.github.com>
2023-01-11 04:16:32 +0400

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -17,3 +17,24 @@ function print_log() {
 DATE=$(date +"%Y-%m-%d %H:%M")
 echo "$DATE $@"
 }
+
+#shortest
+
+function shortest() {
+  min_length=${#1}
+  min_string=$1
+  for arg in "$@"; do
+    if [ ${#arg} -lt $min_length ]; then
+      min_length=${#arg}
+      min_string=$arg
+    fi
+  done
+
+  if [ $# -gt 2 ]; then
+    for arg in "$@"; do
+      echo $arg
+    done | grep -E "^.{$min_length}$"
+  else
+    echo $min_string
+  fi
+}
