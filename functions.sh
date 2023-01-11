4df9c4eb6c8e83f3f9cd2a025fa4e7534a577aba
EdDanielyan <102631130+EdDanielyan@users.noreply.github.com>
2023-01-11 04:01:47 +0400

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 # Put your code here
+
+#pow function
+
+function pow() {
+a=$1
+b=$2
+result=$((a**b))
+echo "$result"
+}
+
+#date and some string
+
+function print_log() {
+DATE=$(date +"%Y-%m-%d %H:%M")
+echo "$DATE $@"
+}
